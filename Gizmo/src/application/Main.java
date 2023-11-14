10c483d0339532e31385ca7e424da11fb69a0904
Parichay Didwania <parichaydidwania@gmail.com>
2023-11-14 16:22:17 -0400

    login page

--- Gizmo/src/application/Main.java
+++ Gizmo/src/application/Main.java
@@ -1,17 +1,22 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 public class Main extends Application {
+		
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			TextField userid = (TextField) root.lookup("#userid");
+			userid.setText("abcd");
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
