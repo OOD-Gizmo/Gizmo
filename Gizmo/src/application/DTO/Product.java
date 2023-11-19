81ce3f6eb52adbebdd0a8a1db81c56f6fd4e7e59
Parichay Didwania <parichaydidwania@gmail.com>
2023-11-18 21:15:12 -0400

    feat: complete seller page

--- Gizmo/src/application/DTO/Product.java
+++ Gizmo/src/application/DTO/Product.java
@@ -1,50 +1,48 @@
 package application.DTO;
 
-import java.util.ArrayList;
-
-enum PRODUCT_INFO {
-	IPHONE12 (1, "IPHONE 12", "../", "RAM-Xgb\nblabla");
-	
-	private int id;
-	private String name;
-	private String imagePath;
-	private String specifications;
-	
-	PRODUCT_INFO(int id, String name, String imagePath, String specifications) {
-		this.id = id;
-		this.name = name;
-		this.imagePath = imagePath;
-	}
-	
-	public int getId() {
-		return this.id;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public String getImagePath() {
-		return this.imagePath;
-	}
-
-	public String getSpecifications() {
-		return specifications;
-	}
-}
-
 public class Product {
+	
+	public static enum PRODUCT_INFO {
+		IPHONE12 (1, "IPHONE 12", "iphone12.jpg", "RAM-Xgb\nblabla");
+		
+		private int id;
+		private String name;
+		private String imagePath;
+		private String specifications;
+		
+		PRODUCT_INFO(int id, String name, String imagePath, String specifications) {
+			this.id = id;
+			this.name = name;
+			this.imagePath = imagePath;
+		}
+		
+		public int getId() {
+			return this.id;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public String getImagePath() {
+			return this.imagePath;
+		}
+
+		public String getSpecifications() {
+			return specifications;
+		}
+	}
 	
 	private PRODUCT_INFO productInfo;
 	private int price;
 	private int stock;
-	private ArrayList<Integer> rating;
+	private double rating;
 	
-	public Product(PRODUCT_INFO productInfo, String specifications, int price, int stock) {
+	public Product(PRODUCT_INFO productInfo, int price, int stock) {
 			this.productInfo = productInfo;
 			this.price = price;
 			this.stock = stock;
-			this.rating = new ArrayList<>();
+			this.rating = 0;
 	}
 	
 	public PRODUCT_INFO getProductId() { // can access id, name, image, specifications
@@ -64,15 +62,6 @@ public class Product {
 	}
 
 	public double getRating() {
-		int sum = 0;
-		for(int i = 0; i < this.rating.size(); i++) {
-			sum += this.rating.get(i);
-		}
-		
-		return sum/this.rating.size();
-	}
-
-	public void addRating(int rate) {
-		this.rating.add(rate);
+		return this.rating;
 	}
 }
