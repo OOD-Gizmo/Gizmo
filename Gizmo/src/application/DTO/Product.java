8341c620a78301fc54c2b44d989f6b714b4f6538
Sampurna <sampurng@gmail.com>
2023-12-02 18:13:52 -0500

    Fixed Products.fxml

--- Gizmo/src/application/DTO/Product.java
+++ Gizmo/src/application/DTO/Product.java
@@ -1,9 +1,11 @@
 package application.DTO;
 
+import javax.imageio.ImageIO;
+
 public class Product {
 	
 	public static enum PRODUCT_INFO {
-		IPHONE12 (1, "IPHONE 12", "iphone12.png", "RAM-Xgb\nblabla");
+		IPHONE12 (1, "IPHONE 12", "Images/iphone12.jpg", "RAM-Xgb\nblabla");
 		
 		private int id;
 		private String name;
