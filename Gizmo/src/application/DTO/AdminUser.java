de8910873f0e8765f5ab056bf0ff943f3ac6d69f
Parichay Didwania <parichaydidwania@gmail.com>
2023-12-09 17:20:46 -0400

    cleanup

--- Gizmo/src/application/DTO/AdminUser.java
+++
@@ -1,9 +0,0 @@
-package application.DTO;
-
-public class AdminUser extends User{
-	
-	public AdminUser(String firstName, String lastName, String userID, String emailID, ContactDetails contactDetails) {
-		// TODO Auto-generated constructor stub
-		super(firstName, lastName, userID, emailID, contactDetails, USER_TYPE.ADMIN);
-			}
-}
